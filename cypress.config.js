695dd13711b598c1ba6efc613f53143028642a27
alegullo <alexandre.gullo@hotmail.com>
2025-06-12 12:25:16 -0300

    add report html

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
+const cypress = require("cypress");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter:'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/results/junit/results-[hash].xml',
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts:true,
+      reportPageTitle: 'Cypress Test Report',
+      embeddedScreens: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
+  },
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
