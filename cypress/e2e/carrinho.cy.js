23f3cc1f1a529e365921b894055f64457c66418f
alegullo <alexandre.gullo@hotmail.com>
2025-06-12 11:55:47 -0300

    add cart with sucess

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+/// <reference types="cypress" />
+
+describe('Carrinho', () => {
+  beforeEach(() => {
+    cy.login('standard_user', 'secret_sauce');
+  });
+  
+  it('Adicionar 1 produto ao carrinho com sucesso', () => {
+    cy.get('.inventory_item').first().find('.btn_inventory').click();
+    cy.get('.shopping_cart_container').should('contain', '1');
+    cy.get('.shopping_cart_container').click();
+
+    cy.contains('Sauce Labs Backpack').should('be.visible');
+  });
+  
 })
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -10,15 +10,4 @@ describe('Logout', () => {
     cy.url().should('include', '/');
     cy.get('#login-button').should('be.visible');
   });
-  it('Deve redirecionar para a página de login após logout', () => {
-    cy.get('#react-burger-menu-btn').click();
-    cy.get('#logout_sidebar_link').click();
-    cy.url().should('include', '/');
-    cy.get('#login-button').should('be.visible');
-  });
-  it('Deve exibir o botão de login após logout', () => {
-    cy.get('#react-burger-menu-btn').click();
-    cy.get('#logout_sidebar_link').click();
-    cy.get('#login-button').should('be.visible');
-  });
 });
